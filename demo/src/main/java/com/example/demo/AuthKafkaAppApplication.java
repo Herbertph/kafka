ab85acb89ba48c0bd1f259f5556b262c05a32502
Herbert Parasio <91493748+Herbertph@users.noreply.github.com>
2025-01-13 09:22:04 -0400

    Setup Done

---
+++ demo/src/main/java/com/example/demo/AuthKafkaAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthKafkaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthKafkaAppApplication.class, args);
+	}
+
+}
