a685334251fd7bbd7fb78d4d78e26f61951c9978
Herbert Parasio <91493748+Herbertph@users.noreply.github.com>
2025-01-15 08:40:15 -0400

    DB Configuration Done
    Mysql

--- demo/src/main/java/com/example/demo/AuthKafkaAppApplication.java
+++ demo/src/main/java/com/example/demo/AuthKafkaAppApplication.java
@@ -2,12 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class AuthKafkaAppApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure().load();
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
+
 		SpringApplication.run(AuthKafkaAppApplication.class, args);
 	}
-
 }
